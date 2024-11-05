cc098c1964cd28decb356227585d3d85b35694df
ZEP13 <zephyr.lathuy.zpl@gmail.com>
2024-11-05 15:35:27 +0100

    Update index.js

--- CV_2/index.js
+++ CV_2/index.js
@@ -1,3 +1,4 @@
+const btnspan = document.querySelector(".navspan");
 const btnspan1 = document.querySelector(".navspan1");
 const btnspan2 = document.querySelector(".navspan2");
 const btnspan3 = document.querySelector(".navspan3");
@@ -5,7 +6,7 @@ const boxforma = document.querySelector(".boxform");
 const boxexp = document.querySelector(".boxexp");
 const boxcontact = document.querySelector(".boxcontact");
 const profilbox = document.querySelector(".profil");
-const home = document.querySelector(".navspan");
+const home = document.querySelector(".boxhome");
 const profil = document.querySelector(".profil");
 const navbox = document.querySelector(".navbox");
 
@@ -27,15 +28,30 @@ btnspan3.addEventListener("click", () => {
   navbox.classList.add("hid-box");
 });
 
+btnspan.addEventListener("click", () => {
+  home.classList.add("show-box");
+  profilbox.classList.add("hid-box");
+  navbox.classList.add("hid-box");
+});
+
 //const boxnav = document.querySelector(".navbox");
 //boxnav.forEach((boxnav) => {
 // boxnav.addEventListener("click", (e) => {
 //  boxnav.style.transform = "scale(0.7)";
 // });
 //});
+const closehome = document.querySelector(".closebox");
 const closebox = document.querySelector(".closebox1");
 const closebox1 = document.querySelector(".closebox2");
 const closebox2 = document.querySelector(".closebox3");
+const boxhome = document.querySelector(".boxhome");
+
+closehome.addEventListener("click", () => {
+  boxhome.classList.remove("show-box");
+  profilbox.classList.remove("hid-box");
+  navbox.classList.remove("hid-box");
+});
+
 closebox2.addEventListener("click", () => {
   boxcontact.classList.remove("show-box");
   profilbox.classList.remove("hid-box");
@@ -53,3 +69,4 @@ closebox.addEventListener("click", () => {
   profilbox.classList.remove("hid-box");
   navbox.classList.remove("hid-box");
 });
+
